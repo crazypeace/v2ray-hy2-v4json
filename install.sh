338d24e64121beb2eeeec2bf97e4d4fd912d9135
crazypeace <txican@gmail.com>
2024-12-08 15:52:37 +0800

    if [[ $netstack == "6" ]]; then   ip=[$ip] fi
    if [[ $netstack == "6" ]]; then
      ip=[$ip]
    fi

--- install.sh
+++ install.sh
@@ -361,6 +361,9 @@ echo -e "$yellow 应用层协议协商 (Alpn) = ${cyan}h3${none}"
 echo -e "$yellow 跳过证书验证 (allowInsecure) = ${cyan}true${none}"
 echo
 echo "---------- 链接 URL ----------"
+if [[ $netstack == "6" ]]; then
+  ip=[$ip]
+fi
 v2ray_hy2_url="hysteria2://${pwd}@${ip}:${port}?alpn=h3&insecure=1#HY2-${ip}"
 echo -e "${cyan}${v2ray_hy2_url}${none}"
 echo
